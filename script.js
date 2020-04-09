db7fee41cff659d634c7b5033b8ccb5a22ca50ab
Charles Ivia <iviacharles@gmail.com>
2020-04-09 13:10:55 +0300

    Updated the javacript.js file

--- script.js
+++ script.js
@@ -1,31 +1,146 @@
-let myLibrary = [];
-let addBook;
-let libraryRender;
-let removeBook;
-let booksCreated = 3;
+const table = document.querySelector('tbody');
+const addTitle = document.getElementById('addTitle');
+const addAuthor = document.getElementById('addAuthor');
+const addPages = document.getElementById('addPages');
+const addRead = document.getElementById('addRead');
+const addButton = document.querySelector('.addBookBtn');
+const removeButton = document.querySelector('.remove');
+const toggleAddBook = document.querySelector('.showBookForm');
+const addForm = document.querySelector('.bookForm');
 
-const deleteBtns = document.querySelectorAll('.del-btn');
-const addBtn = document.querySelector('.add-btn');
-let libraryTable = document.querySelector('.library-list');
-const confirmBtn = document.querySelector('.confirm-btn')
+addButton.addEventListener('click', addBookBar);
+toggleAddBook.addEventListener('click', showAddBook);
 
-deleteBtns.forEach(button => button.addEventListener('click', removeBook));
-
-confirmBtn.addEventListener('click', button => {
-
-});
-
-addBtn.addEventListener('click', button => {
-    
-});
-
-function Book(title, author, pages, status, action) {
-    this.id = booksCreated;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.action = action;
-
-    booksCreated+=1
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 };
+
+let myLibrary = [];
+
+addBooktoLibrary('The Great Gatsby', 'F.Scot Fitzgerald', 271, true);
+addBooktoLibrary('Antifragile Things', 'Nassim Taleb', 675, true);
+addBooktoLibrary('The Way of Kings', 'Brandon Sanderson', 12375, false);
+
+function addBooktoLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    clearTable();
+    render();
+};
+
+function render() {
+    //Grabs each book
+    myLibrary.forEach(element => {
+      let newRow = document.createElement('tr');
+      newRow.setAttribute('class', 'book_' + myLibrary.indexOf(element));
+  
+      //Grabs books key
+      for (const key in element) {
+        let newData = document.createElement('td');
+  
+        //Makes Read Button
+        if (key == 'read') {
+          let readButton = document.createElement('input');
+          readButton.type = 'button';
+          readButton.addEventListener('click', () => toggleRead(newRow));
+  
+          if (element[key]) {
+            readButton.value = 'Read';
+            readButton.setAttribute('class', 'bookRead');
+          } else {
+            readButton.value = 'Not Read';
+            readButton.setAttribute('class', 'bookUnRead');
+          }
+          newData.appendChild(readButton);
+          newRow.appendChild(newData);
+          //Fills rest of info
+        } else {
+          newData.innerHTML = element[key];
+          newRow.appendChild(newData);
+        }
+      }
+      table.appendChild(newRow);
+    });
+    addRemoveButton();
+  };
+
+  function clearTable() {
+      while (table.hasChildNodes()) {
+          table.removeChild(table.lastChild);
+      };
+  };
+
+  function addBookBar() {
+    let newTitle = addTitle.value;
+    let newAuthor = addAuthor.value;
+    let newPages = addPages.value;
+    let newRead;
+    if (addRead.value === 'true') {
+      newRead = true;
+    } else {
+      newRead = false;
+    }
+    //If inputs invalid returns
+    if (newTitle === '' || newAuthor === '' || newPages === '' || hasNumbers(newAuthor) || hasLetters(newPages)) {
+      document.querySelector('.warning').setAttribute('style', 'display:block');
+      return;
+    }
+  
+    document.querySelector('.warning').setAttribute('style', 'display:none');
+    addBooktoLibrary(newTitle, newAuthor, newPages, newRead);
+    showAddBook();
+  }
+  
+  function addRemoveButton() {
+    let amount = myLibrary.length;
+  
+    for (let i = 0; i < amount; i++) {
+      let tableRow = document.querySelector('.book_' + i);
+      let makeTD = document.createElement('td');
+      let deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+      deleteButton.value = 'Remove';
+      deleteButton.addEventListener('click', () => deleteBook(tableRow));
+      makeTD.appendChild(deleteButton);
+      tableRow.appendChild(makeTD);
+    }
+  }
+  
+  function deleteBook(element) {
+    let bookIndex = element.className.slice(5, 6);
+  
+    myLibrary.splice(bookIndex, 1);
+    clearTable();
+    render();
+  }
+  
+  function toggleRead(element) {
+    let bookIndex = element.className.slice(5, 6);
+    let readStatus = myLibrary[bookIndex].read;
+    let button = element.querySelector('input');
+  
+    if (readStatus) {
+      myLibrary[bookIndex].read = false;
+      button.setAttribute('class', 'Not Read');
+      button.setAttribute('value', 'Not Read');
+    } else {
+      myLibrary[bookIndex].read = true;
+      button.setAttribute('class', 'Read');
+      button.setAttribute('value', 'Read');
+    }
+  }
+  
+  function showAddBook() {
+    if (addForm.style.display == 'flex') {
+      addForm.setAttribute('style', 'display: none');
+    } else {
+      addForm.setAttribute('style', 'display: flex', 'margin-top: 90px');
+    }
+  }
+  
+ 
